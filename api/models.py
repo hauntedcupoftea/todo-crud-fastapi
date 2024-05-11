b4cdfe342fdd067b7fed614d93502e669909d451
hauntedcupoftea <andydchauhan@gmail.com>
2024-05-11 15:45:36 +0530

    consolidate db stuff to one file

--- api/models.py
+++ api/models.py
@@ -1,16 +1,40 @@
-from uuid import uuid4
-from tortoise.models import Model
-from tortoise.fields import (
-    CharField, UUIDField, BooleanField, DatetimeField
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import dotenv_values
+
+values = dotenv_values('./test.env')
+SQLALCHEMY_DATABASE_URL = values['DB_CONNECTION']
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
 )
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-class Todo(Model):
-    id = UUIDField(pk=True, default=uuid4)
-    title = CharField(max_length=100, null=False)
-    done = BooleanField(null=False, default=False)
-    created_at = DatetimeField(null=True, auto_now_add=True)
+Base = declarative_base()
 
-    class Meta:
-        table = 'todos'  # table name on the db
-        ordering = ['-created_at']  # order by c_at DESC
+class Todo(Base):
+    __tablename__ = "todo_table"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(25))
+    desc = Column(String(100))
+    user_id = Column(Integer, ForeignKey('user_table.id'))
+    creator = relationship("User", back_populates="todo_table")
+
+class User(Base):
+    __tablename__ = "user_table"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100))
+    email = Column(String(100))
+    password = Column(String(100))
+    todo_table = relationship("Todo", back_populates="creator")
