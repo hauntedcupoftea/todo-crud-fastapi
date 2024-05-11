9ac9b1bcccd5888879b4b5f31b5ec55baa8bcb4e
hauntedcupoftea <andydchauhan@gmail.com>
2024-05-11 14:27:22 +0530

    restructure project for the better

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-import uvicorn
+from fastapi import FastAPI
+from api.routing import router as todo_router
+from tortoise.contrib.fastapi import register_tortoise
+from settings.configuration import Config
 
-if __name__ == "__main__":
-    uvicorn.run("todo.main:app")
+app = FastAPI()
+app.include_router(todo_router)
+register_tortoise(
+    app=app,
+    db_url=Config.DB_CONNECTION,
+    modules={'models': Config.DB_MODELS},
+    add_exception_handlers=True,
+    generate_schemas=False
+)
+
+
+@app.get('/')
+def home():
+    return "Welcome to todos api"
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(
+        app=app,
+    )
